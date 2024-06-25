4895b1e0c237c9dd3f067078b79f4bdffff1cd31
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-06-24 20:31:02 -0500

    Delete jenkins-shared-library directory

--- jenkins-shared-library/deployToEKS.groovy
+++
@@ -1,5 +0,0 @@
-def call(String helmChartPath, String releaseName, String namespace) {
-    stage('Deploy to EKS') {
-        sh "helm upgrade --install ${releaseName} ${helmChartPath} --namespace ${namespace} --set image.tag=${env.BUILD_NUMBER}"
-    }
-}
--- jenkins-shared-library/dockerBuildAndPush.groovy
+++
@@ -1,7 +0,0 @@
-def call(String ecrRepoUrl) {
-    stage('Docker Build & Push') {
-        def imageName = "${ecrRepoUrl}:${env.BUILD_NUMBER}"
-        sh "docker build -t ${imageName} ."
-        sh "docker push ${imageName}"
-    }
-}
